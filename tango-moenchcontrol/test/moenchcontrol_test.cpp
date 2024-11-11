0a21a5c90f4d5b3d713d9224b2f6971f46daebff
lrlunin <lunin@campus.tu-berlin.de>
2024-11-11 19:19:45 +0100

    temporary disabled moenchControl tango tests

--- tango-moenchcontrol/test/moenchcontrol_test.cpp
+++ tango-moenchcontrol/test/moenchcontrol_test.cpp
@@ -1,6 +1,24 @@
-#include <gtest/gtest.h>
-
 #include "../src/tangods/MoenchControl.h"
+#include "../src/tangods/MoenchControlClass.h"
+#include <gtest/gtest.h>
+#include <iostream>
 #include <tango/tango.h>
 
-TEST(MoenchControlTest, TypeAssertions) { SUCCEED(); }
+class MoenchControlTest : public testing::Test {
+protected:
+  Tango::DeviceProxy *dp;
+
+  void SetUp() {
+    dp = new Tango::DeviceProxy("127.0.0.1:50009/a/b/c#dbase=no");
+  };
+
+  void TearDown() { delete dp; };
+};
+
+TEST_F(MoenchControlTest, basisTest) {
+  std::cout << "TEST" << std::endl;
+  Tango::DevFloat exptime;
+  dp->read_attribute("exposure") >> exptime;
+  std::cout << "Exptime: " << exptime << std::endl;
+  SUCCEED();
+}
